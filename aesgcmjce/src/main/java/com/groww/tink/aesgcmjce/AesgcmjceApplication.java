45d4bcfd0f5d3afb24049cc4234cba8a0799f8c8
Mohamed Sameem <42725415+MOHAMEDSAMEEM77@users.noreply.github.com>
2022-09-13 16:05:47 +0530

    updates

---
+++ aesgcmjce/src/main/java/com/groww/tink/aesgcmjce/AesgcmjceApplication.java
@@ -0,0 +1,13 @@
+package com.groww.tink.aesgcmjce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AesgcmjceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AesgcmjceApplication.class, args);
+	}
+
+}
